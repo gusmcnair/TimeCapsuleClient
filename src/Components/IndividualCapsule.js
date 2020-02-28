d8cbd28976f9f989bed08eb3f501d5ddaac21563
Angus McNair <amcnair234@gmail.com>
2020-02-27 20:39:57 -0600

    Additional styling

--- src/Components/IndividualCapsule.js
+++ src/Components/IndividualCapsule.js
@@ -125,7 +125,7 @@ export default class IndividualCapsule extends React.Component {
             <article>
                 <div className='capsule'>
                 <div className='textcontainer'>
-                    <button disabled={this.state.disabled} onClick={e => this.handleOpen(this.props.id)} className='opencapsule' id={this.props.title + '_button'}>{this.state.clock}<span className='opentext'>{this.state.status}</span></button>
+                    <button disabled={this.state.disabled} onClick={e => this.handleOpen(this.props.id)} className='opencapsule' id={this.props.title + '_button'}>{this.state.clock}{this.state.status}</button>
                     </div>
                     <div className='textcontainer'>
                         <div className='insidecontainer'>
